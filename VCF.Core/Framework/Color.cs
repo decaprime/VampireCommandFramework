a101f55522199f87ac6d75667a6ed0e3e239237f
Amber <odjiti@gmail.com>
2025-05-17 14:20:09 -0400

    When you submit a command but its not quite right up to three of the closest possible command matches are returned.
    Updating presentation of messages and help.
    Updating unit tests for the new presentation of messages

--- VCF.Core/Framework/Color.cs
+++ VCF.Core/Framework/Color.cs
@@ -6,10 +6,23 @@
 public static class Color
 {
 	public static string Red = "red";
-	public static string Primary = "#b0b";
+	public static string Primary = "#d25";
 	public static string White = "#eee";
-	public static string LightGrey = "#ccc";
+	public static string LightGrey = "#bbf";
 	public static string Yellow = "#dd0";
-	public static string DarkGreen = "#0c0";
-	public static string Command = "#40E0D0";
+	public static string DarkGreen = "#4c4";
+	public static string Command = "#4ED";
+	public static string Beige = "#fed";
+	public static string Gold = "#eb0";
+	public static string Magenta = "#c5d";
+	public static string Pink = "#faf";
+	public static string Purple = "#52d";
+	public static string LightBlue = "#3ac";
+	public static string LightRed = "#f45";
+	public static string LightPurple = "#84c";
+	public static string Lilac = "#b9f";
+	public static string LightPink = "#EED";
+	public static string Grey = "#eef";
+
+
 }
